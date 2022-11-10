ff18231b3d95cee601cca8c5bfa396c773dbfebc
Giulliana Rangel <giulliana.rangel@gmail.com>
2022-11-10 15:44:47 -0300

    feat: adiciona testes

---
+++ src/tests/Login.test.js
@@ -0,0 +1,9 @@
+import App from "../App";
+import userEvent from "@testing-library/user-event";
+import { renderWithRouterAndRedux } from './helpers';
+
+describe('Verifica se os testes possuem cobertura > 90%', () => {
+  test('', () => {
+
+  });
+});
