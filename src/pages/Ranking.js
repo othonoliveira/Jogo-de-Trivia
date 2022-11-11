8b15c7be9a24a9e29b2f1960bc5ffa1019f233a1
Giulliana Rangel <giulliana.rangel@gmail.com>
2022-11-11 09:16:18 -0300

    feat: add req 16 (btn-ranking)

---
+++ src/pages/Ranking.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Ranking extends React.Component {
+  render() {
+    return (
+      <title data-testid="ranking-title">Ranking</title>
+    );
+  }
+}
+export default Ranking;
