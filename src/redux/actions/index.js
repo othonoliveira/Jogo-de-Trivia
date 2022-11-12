d68674d06f5716a106cd8e3380ded389f68504b6
Rian Alves <riansa0902@gmail.com>
2022-11-12 11:23:41 -0300

    feat[project]: Requisito 19 *Adiciona sistema de ranqueamento, e salva no localStorage

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,6 +3,8 @@ export const SAVE_PLAYER = 'SAVE_PLAYER';
 
 export const SAVE_POINTS = 'SAVE_POINTS';
 
+export const CLEAR_SCORE = 'CLEAR_SCORE';
+
 // action creator
 export const savePlayer = (payload) => ({
   type: SAVE_PLAYER,
@@ -12,4 +14,8 @@ export const savePlayer = (payload) => ({
 export const savePoints = (payload) => ({
   type: SAVE_POINTS,
   payload,
+});
+
+export const restartScore = () => ({
+  type: CLEAR_SCORE,
 });
